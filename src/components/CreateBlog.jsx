dfb914f991a2c597e0c179d7e498dc8d7d1f2ff6
doraeem <mehzebin.meem@gmail.com>
2024-11-16 14:08:13 +0600

    added blog

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import Editor from 'react-simple-wysiwyg';
+import { useForm } from 'react-hook-form';
 
 const CreateBlog = () => {
-   const [html, setHtml] = useState('my <b>HTML</b>');
+   const [html, setHtml] = useState('');
 
    function onChange(e) {
     setHtml(e.target.value);
    }
+
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const formSubmit = async (data) => {
+    const newData = {...data, "description":html}
+    // console.log(newData);
+    const res = await fetch("http://localhost:8000/api/blogs",{
+      method:"POST",
+      headers:{
+        'Content-type' : 'application/json'
+      },
+      body : JSON.stringify(newData)
+    });
+
+  }
 
 
   return (
@@ -16,27 +37,42 @@ const CreateBlog = () => {
       <a href="/" className='btn btn-dark'>Back</a>
     </div>
     <div className="card border-0 shadow-lg">
+      <form onSubmit={handleSubmit(formSubmit)}>
        <div className="card-body">
        <div className="mb-3">
-            <label htmlFor="" className='form-label'>Title</label>
-            <input type="text" className='form-control' placeholder='Title'/>
+            <label className='form-label'>Title</label>
+            <input {...register('title',{required: true})}
+            type="text" 
+            className={`form-control ${errors.title && 'is-invalid'}`} 
+            placeholder='Title'/>
+            {errors.title && <p className='invalid-feedback'>Title field is required</p>}
         </div>
         <div className="mb-3">
-            <label htmlFor="" className='form-label'>Description</label>
+            <label className='form-label'>Short Description</label>
+            <textarea {...register('shortDesc')}
+            cols="30" rows="5" className='form-control'></textarea>
+        </div>
+        <div className="mb-3">
+            <label className='form-label'>Description</label>
             <Editor value={html} containerProps={{ style: { height: '400px' } }}
             onChange={onChange} />
            
         </div>
         <div className="mb-3">
-            <label htmlFor="" className='form-label'>Image</label> <br/>
+            <label className='form-label'>Image</label> <br/>
             <input type="file" />
         </div>
         <div className="mb-3">
-            <label htmlFor="" className='form-label'>Author</label>
-            <input type="text" className='form-control' placeholder='Author'/>
+            <label className='form-label'>Author</label>
+            <input {...register('author',{required: true})}
+            type="text" 
+            className={`form-control ${errors.author && 'is-invalid'}`} 
+            placeholder='Author'/>
+            {errors.author && <p className='invalid-feedback'>Author field is required</p>}
         </div>
         <button className='btn btn-dark'>Create</button>
        </div>
+       </form>
 
     </div>
     </div>
